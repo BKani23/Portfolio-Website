fcefb3cb11b9a349e0b7d4035171d64f27b161f9
BKani23 <bkani2304@gmail.com>
2026-02-25 14:43:19 +0200

    feat : implement the scoll down indicator

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,31 @@
-import React from "react";
 import "../styles/Home.css"; 
+
 
 const Home = () => {
   return (
-    <section className="home-section">
-      <div className="overlay">
-        <div className="home-content">
-          <h1 className="home-name">Bulelani Kani.</h1>
-          <h2 className="home-title">Fullstack Software Developer</h2>
-        </div>
+    <section
+      className="home-section"
+    >
+      <div className="overlay"></div>
+
+      {/* Hero content */}
+      <div className="home-content">
+        <h1 className="home-name">Bulelani Kani<span className="period">.</span></h1>
+        <h2 className="home-title">Fullstack Software Developer</h2>
+      </div>
+
+      {/* Scroll-down indicator */}
+      <div
+        className="scroll-down"
+        onClick={() =>
+        {
+          document
+            .getElementById("about")
+            .scrollIntoView({ behavior: "smooth" })
+        }
+      }
+      >
+        <span></span>
       </div>
     </section>
   );
