4a106100b2d3154a63edffe1bfafc7364fdacdaf
BKani23 <bkani2304@gmail.com>
2026-02-27 14:48:09 +0200

    feat: Import and integrate Loading component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import About from "./components/About";
 import SocialIcons from "./components/SocialIcons";
+import Loading from "./components/Loading";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <>
-      <Header />
-      <Home />
-      <About />
-      <SocialIcons/>
+      {isLoading && <Loading onFinish={() => setIsLoading(false)} />}
+
+      {!isLoading && (
+        <>
+          <Header />
+          <Home />
+          <About />
+          <SocialIcons />
+        </>
+      )}
     </>
   );
 }
